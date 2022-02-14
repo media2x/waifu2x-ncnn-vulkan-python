16c4a254d5dc7ada3bd7dfbd77e168da59d26081
ArchieMeng <archiemeng@protonmail.com>
2022-02-14 20:19:43 +0800

    Update setup.py and CMakeLists.txt to allow building with specific python version
    For example (Linux platform):
    env CMAKE_FLAGS="-DPY_VERSION=3.9" python -m build -w

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
 import pathlib
 import setuptools
 
 import cmake_build_extension
+
+
+cmake_flags = [
+    "-DBUILD_SHARED_LIBS:BOOL=OFF",
+    "-DCALL_FROM_SETUP_PY:BOOL=ON",
+]
+cmake_flags.extend(os.environ.get("CMAKE_FLAGS", "").split())
 
 setuptools.setup(
     ext_modules=[
@@ -14,10 +22,7 @@ setuptools.setup(
             source_dir=str(
                 pathlib.Path(__file__).parent / "waifu2x_ncnn_vulkan_python"
             ),
-            cmake_configure_options=[
-                "-DBUILD_SHARED_LIBS:BOOL=OFF",
-                "-DCALL_FROM_SETUP_PY:BOOL=ON",
-            ],
+            cmake_configure_options=cmake_flags,
         )
     ],
     cmdclass={"build_ext": cmake_build_extension.BuildExtension},
